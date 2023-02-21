64b47e05696cb18d227b4a842ded3af785140ff2
azizamari <azizamari2003@gmail.com>
2023-02-21 14:43:06 +0100

    create shared folder and add exception handling

--- API/src/app.module.ts
+++ API/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import {MongooseModule} from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [ProductsModule, UsersModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/JE')],
+  imports: [ProductsModule, UsersModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/JE'), SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
